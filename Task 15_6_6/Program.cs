fee067fa5e923eb1a687509986ba5dafdcd67f49
Варнавский <@DESKTOP-OLMIPJ9>
2023-01-25 16:30:39 +0300

    Добавьте файлы проекта.

---
+++ Task 15_6_6/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task_15_6_6
+{
+    public class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var result = from student in classes
+                         from name in student.Students
+                         select name.ToString();
+            return result.ToArray();
+        }
+    }
+}
